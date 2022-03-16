6f70e21df7dc41c06b2ec160b875b454c91d22f9
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-16 15:37:20 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-/**/
+/*Menu home toggle btn*/
 const bar = document.getElementById('bar');
-
+const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
 if(bar){
     bar.addEventListener('click', () => {
         nav.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
     })
 }
