5a85310d314f7a0c10bd03369fd40546c34e24ab
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-16 15:26:57 -0300

    Update

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/**/
+const bar = document.getElementById('bar');
+
+const nav = document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
